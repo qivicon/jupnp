346ee9591be612d78ea8b524f566fe8e36f4ee55
velichko.stoykov <velichko.stoykov@musala.com>
2018-03-15 13:25:49 +0200

    Set max connections per destination

--- bundles/org.jupnp/src/main/java/org/jupnp/transport/impl/jetty/JettyStreamClientImpl.java
+++ bundles/org.jupnp/src/main/java/org/jupnp/transport/impl/jetty/JettyStreamClientImpl.java
@@ -63,6 +63,7 @@ public class JettyStreamClientImpl extends AbstractStreamClient<StreamClientConf
         // These are some safety settings, we should never run into these timeouts as we
         // do our own expiration checking
         httpClient.setConnectTimeout((getConfiguration().getTimeoutSeconds() + 5) * 1000);
+        httpClient.setMaxConnectionsPerDestination(2);
 
         final QueuedThreadPool queuedThreadPool = createThreadPool("jupnp-jetty-client", 5, 5, 60000);
 
